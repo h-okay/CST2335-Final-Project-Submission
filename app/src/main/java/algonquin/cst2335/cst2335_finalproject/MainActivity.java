d49fc941a6dbddebbef8a3a12fea9531d00955a1
Iseul Park <park0586@algonquinlive.com>
2023-07-15 21:45:26 -0400

    update sharedPreferences for the editText and the enter Button

--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/MainActivity.java
@@ -2,13 +2,46 @@ package algonquin.cst2335.cst2335_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button enterButton;
+    private EditText typeAirportCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        typeAirportCode = findViewById(R.id.typeAirportCode);
+        typeAirportCode.setText(getSavedEditTextValue("airportCode", ""));
+
+        enterButton = findViewById(R.id.enterButton);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String airportCode = typeAirportCode.getText().toString();
+                saveEditTextValue("airportCode", airportCode);
+               // Toast.makeText(getApplicationContext(), "Airport code saved!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void saveEditTextValue(String airportCode, String value) {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(airportCode, value);
+        editor.apply();
+    }
+
+    private String getSavedEditTextValue(String airportCode, String defaultValue) {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        return sharedPreferences.getString(airportCode, defaultValue);
     }
 }
