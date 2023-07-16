101b6ef6af3b846f2ea736d608cc45bf69938bf3
Iseul Park <park0586@algonquinlive.com>
2023-07-15 21:49:48 -0400

    add comments

--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         typeAirportCode = findViewById(R.id.typeAirportCode);
+        // Retrieve and set the saved EditText value
         typeAirportCode.setText(getSavedEditTextValue("airportCode", ""));
 
         enterButton = findViewById(R.id.enterButton);
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String airportCode = typeAirportCode.getText().toString();
+                // Save the EditText value
                 saveEditTextValue("airportCode", airportCode);
                // Toast.makeText(getApplicationContext(), "Airport code saved!", Toast.LENGTH_SHORT).show();
             }
@@ -36,12 +38,14 @@ public class MainActivity extends AppCompatActivity {
     private void saveEditTextValue(String airportCode, String value) {
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
+        // Save the EditText value with the provided key
         editor.putString(airportCode, value);
         editor.apply();
     }
 
     private String getSavedEditTextValue(String airportCode, String defaultValue) {
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        // Retrieve the saved EditText value using the key
         return sharedPreferences.getString(airportCode, defaultValue);
     }
 }
