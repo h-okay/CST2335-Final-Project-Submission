c7294013fe534a7e505867f9317671663e6f29fe
Hakan Okay <hokay18@gmail.com>
2023-07-20 00:55:22 -0400

    [Bear]Added SharedPreferences functionality

--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/MainActivity.java
@@ -1,9 +1,13 @@
 package algonquin.cst2335.cst2335_finalproject;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,7 +17,9 @@ import algonquin.cst2335.cst2335_finalproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
+    private SharedPreferences prefs;
+    private ActivityResultLauncher<Intent> bearApplicationLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,35 +27,39 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
+
+        prefs = getSharedPreferences("BearAppData", Context.MODE_PRIVATE);
+
+        bearApplicationLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        int height = result.getData().getIntExtra("bearHeight", 0);
+                        int width = result.getData().getIntExtra("bearWidth", 0);
+
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putInt("bearHeight", height);
+                        editor.putInt("bearWidth", width);
+                        editor.apply();
+                    }
+                }
+        );
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.app_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         int itemId = item.getItemId();
-        if (itemId == R.id.aviation) {
-            //Intent intent = new Intent(MainActivity.this, AviationApplication.class);
-            //startActivity(intent);
-        }
-        if (itemId == R.id.trivia) {
-            //Intent intent = new Intent(MainActivity.this, TriviaApplication.class);
-            //startActivity(intent);
-        }
         if (itemId == R.id.bear) {
             Intent intent = new Intent(MainActivity.this, BearApplication.class);
-            startActivity(intent);
+            bearApplicationLauncher.launch(intent);
+            return true;
         }
-        if (itemId == R.id.currency) {
-            //Intent intent = new Intent(MainActivity.this, CurrencyApplication.class);
-            //startActivity(intent);
-        }
-        return true;
+        return super.onOptionsItemSelected(item);
     }
 }
