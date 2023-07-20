48d37931483ec036878e11fa7bd5fd1eac2fdabc
Iseul Park <park0586@algonquinlive.com>
2023-07-20 12:36:53 -0400

    add delete button for the fragment

--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/FLightListViewModel.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/FLightListViewModel.java
@@ -1,5 +1,6 @@
 package algonquin.cst2335.cst2335_finalproject;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -8,4 +9,19 @@ import java.util.ArrayList;
 public class FLightListViewModel extends ViewModel {
     public MutableLiveData<ArrayList<Flight>> lists = new MutableLiveData<>();
     public MutableLiveData<Flight> selectedList = new MutableLiveData<>();
+
+    public LiveData<ArrayList<Flight>> getFlightListLiveData() {
+        if (lists == null) {
+            lists = new MutableLiveData<>();
+        }
+        return lists;
+    }
+    public void flightDeleted() {
+        // Trigger an update to the LiveData so that observers (FlightMainActivity) can be notified
+        ArrayList<Flight> currentList = lists.getValue();
+        lists.postValue(currentList);
+    }
+
+
+
 }
