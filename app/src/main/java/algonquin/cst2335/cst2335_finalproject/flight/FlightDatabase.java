a449dad1db32e2b1580c1cad47b6cc1070fb32ae
Iseul Park <park0586@algonquinlive.com>
2023-07-27 19:16:10 -0400

    add javadocs to FlightDatabase.java

--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/flight/FlightDatabase.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/flight/FlightDatabase.java
@@ -3,9 +3,18 @@ package algonquin.cst2335.cst2335_finalproject.flight;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+/**
+ * FlightDatabase is an abstract class that extends RoomDatabase and serves as the main access point
+ * to the underlying SQLite database for managing Flight entities.
+ * This class is annotated with @Database, which specifies the entities it manages and the database version.
+ */
 @Database(entities = {Flight.class}, version = 1)
 public abstract class FlightDatabase extends RoomDatabase {
 
+    /**
+     * Abstract method to get the Data Access Object (DAO) for the Flight entity.
+     * @return The FlightDAO instance used to interact with the Flight table in the database.
+     */
     public abstract FlightDAO cmDAO();
 
 }
