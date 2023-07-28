04b083e4c0cbfbe07570689842b42a7d930818e4
Iseul Park <park0586@algonquinlive.com>
2023-07-27 20:35:34 -0400

    add javadocs to FlightAdapter.java

--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/flight/FlightAdapter.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/flight/FlightAdapter.java
@@ -12,27 +12,63 @@ import java.util.List;
 
 import algonquin.cst2335.cst2335_finalproject.R;
 
+/**
+ * RecyclerView Adapter for displaying a list of Flight items in a RecyclerView.
+ * This adapter binds Flight data to the corresponding UI elements in the flight_list layout.
+ */
 public class FlightAdapter extends RecyclerView.Adapter<FlightAdapter.FlightViewHolder> {
+    /**
+     * The list of Flight objects to be displayed in the RecyclerView.
+     */
     private List<Flight> flightList;
+    /**
+     * Interface for handling item click events in the RecyclerView.
+     */
     private OnItemClickListener listener; // for fragment
 
+    /**
+     * Constructor for the FlightAdapter.
+     * @param flightList The list of Flight objects to be displayed in the RecyclerView.
+     */
     public FlightAdapter(List<Flight> flightList) {
         this.flightList = flightList;
     }
 
+    /**
+     * Interface for handling item click events in the RecyclerView.
+     */
     public interface OnItemClickListener {
+        /**
+         * Callback method to handle item click events.
+         * @param flight The Flight object representing the clicked item.
+         */
         void onItemClick(Flight flight);
     }
-
+    /**
+     * Sets the click listener for the RecyclerView items.
+     * @param listener The OnItemClickListener to be set as the click listener.
+     */
     public void setOnItemClickListener(OnItemClickListener listener) {
         this.listener = listener;
     }
 
+    /**
+     * Constructor for the FlightAdapter with an initial list of flights and a click listener.
+     * @param flightList The list of Flight objects to be displayed in the RecyclerView.
+     * @param listener   The OnItemClickListener to handle item click events.
+     */
     public FlightAdapter(List<Flight> flightList, OnItemClickListener listener) {
         this.flightList = flightList;
         this.listener = listener;
     }
 
+    /**
+     * Called when a new ViewHolder is created to represent an item in the RecyclerView.
+     * Inflates the layout for a single flight list item and creates a new FlightViewHolder.
+     * @param parent   The parent ViewGroup that the new View will be attached to.
+     * @param viewType The view type of the new View.
+     * @return A new FlightViewHolder that holds the inflated flight_list item view.
+     */
     @NonNull
     @Override
     public FlightViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -40,6 +76,12 @@ public class FlightAdapter extends RecyclerView.Adapter<FlightAdapter.FlightView
         return new FlightViewHolder(view);
     }
 
+    /**
+     * Called by RecyclerView to display the data at a specific position.
+     * Binds the Flight data to the corresponding UI elements in the ViewHolder.
+     * @param holder   The FlightViewHolder to bind the data to.
+     * @param position The position of the item in the data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull FlightViewHolder holder, int position) {
         Flight flight = flightList.get(position);
@@ -54,11 +96,18 @@ public class FlightAdapter extends RecyclerView.Adapter<FlightAdapter.FlightView
         holder.bind(flight, departureText, flightNumberText, destinationText, terminalText, gateText, delayText);
     }
 
+    /**
+     * Returns the total number of items in the data set.
+     * @return The total number of Flight objects in the flightList.
+     */
     @Override
     public int getItemCount() {
         return flightList.size();
     }
 
+    /**
+     * ViewHolder class representing a single flight list item in the RecyclerView.
+     */
     public class FlightViewHolder extends RecyclerView.ViewHolder {
 
         private TextView departureAirportTextView;
@@ -68,7 +117,11 @@ public class FlightAdapter extends RecyclerView.Adapter<FlightAdapter.FlightView
         private TextView gateTextView;
         private TextView delayTextView;
 
-
+        /**
+         * Constructor for the FlightViewHolder.
+         * Initializes the UI elements and sets a click listener for the item view.
+         * @param itemView The item view representing a single flight list item.
+         */
         public FlightViewHolder(@NonNull View itemView) {
             super(itemView);
             flightNumberTextView = itemView.findViewById(R.id.flightNumber);
@@ -89,10 +142,18 @@ public class FlightAdapter extends RecyclerView.Adapter<FlightAdapter.FlightView
                     }
                 }
             });
-
-
         }
 
+        /**
+         * Binds Flight data to the ViewHolder's UI elements.
+         * @param flight         The Flight object representing the flight list item.
+         * @param departureText  The formatted text for the departure airport.
+         * @param flightNumberText The formatted text for the flight number.
+         * @param destinationText The formatted text for the destination.
+         * @param terminalText   The formatted text for the terminal.
+         * @param gateText       The formatted text for the gate.
+         * @param delayText      The formatted text for the delay.
+         */
         public void bind(Flight flight, String departureText, String flightNumberText, String destinationText, String terminalText, String gateText, String delayText) {
 
             departureAirportTextView.setText(departureText);
