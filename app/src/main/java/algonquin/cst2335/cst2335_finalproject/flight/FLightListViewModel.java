1dcacc010233a881c3eaa574ab464c854a2853fc
Iseul Park <park0586@algonquinlive.com>
2023-07-27 20:30:10 -0400

    add javadocs to FLightListViewModel.java

--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/flight/FLightListViewModel.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/flight/FLightListViewModel.java
@@ -1,13 +1,23 @@
 package algonquin.cst2335.cst2335_finalproject.flight;
 
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
-
+/**
+ * ViewModel class for managing flight-related data in the Flight List screen.
+ * This class extends the Android ViewModel class and provides MutableLiveData objects to hold flight data.
+ */
 public class FLightListViewModel extends ViewModel {
+    /**
+     * MutableLiveData object that holds an ArrayList of Flight objects representing the list of flights.
+     * Observers can be notified of changes in the flight list using this LiveData.
+     */
     public MutableLiveData<ArrayList<Flight>> lists = new MutableLiveData<>();
+    /**
+     * MutableLiveData object that holds a single Flight object representing the currently selected flight.
+     * Observers can be notified of changes in the selected flight using this LiveData.
+     */
     public MutableLiveData<Flight> selectedList = new MutableLiveData<>();
 
 }
